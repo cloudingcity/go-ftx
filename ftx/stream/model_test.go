9d3f191f41dbd6d7bc1843f5bc3c10474a329324
clouding <ghost0436@gmail.com>
2021-05-01 17:42:29 +0800

    test: set location to UTC

--- ftx/stream/model_test.go
+++ ftx/stream/model_test.go
@@ -11,5 +11,5 @@ func TestTime_UnmarshalJSON(t *testing.T) {
 	err := tm.UnmarshalJSON([]byte("1619858251.3361459"))
 
 	assert.NoError(t, err)
-	assert.Equal(t, "2021-05-01 16:37:31", tm.Time.Format("2006-01-02 15:04:05"))
+	assert.Equal(t, "2021-05-01 08:37:31", tm.Time.UTC().Format("2006-01-02 15:04:05"))
 }
